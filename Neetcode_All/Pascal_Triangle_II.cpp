824883eee247222de7d961038ce8cf6b7b23177b
Sagar Patel <sagarp3199@gmail.com>
2023-01-02 00:20:26 -0800

    Added new code

---
+++ Neetcode_All/Pascal_Triangle_II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> ans;
+        vector<int> final;
+
+        for(int i=0;i<=rowIndex;i++)
+        {
+            vector<int> temp(i+1,1);
+            for(int j=1;j<i;j++)
+            {
+                temp[j]=ans[i-1][j]+ans[i-1][j-1];
+            }
+            ans.push_back(temp);
+        }
+        return ans[rowIndex];
+    }
+};
